2cc9448eddcf835d0ec5c239d25fce417b4aeae8
Quentin Tran <quentin.tran@telecom-sudparis.eu>
2022-11-05 09:31:01 +0100

    Add licence and comments

--- UMI3D-Hub/UMI3D-Hub/Services/GitHubService.cs
+++ UMI3D-Hub/UMI3D-Hub/Services/GitHubService.cs
@@ -1,4 +1,17 @@
 ﻿
+/*
+Copyright 2022 Quentin Tran
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -12,12 +25,14 @@ using UMI3DHub.ViewModels;
 
 namespace UMI3DHub.Services
 {
-    internal class GitHubService
+    public class GitHubService
     {
         #region Fields
 
         static HttpClient client = new HttpClient();
+
         public const string apiUrl = "https://api.github.com/repos/";
+
         public const string releasesEndPoint = "/releases";
         public const string lastReleasesEndPoint = "/releases/latest";
 
@@ -25,6 +40,12 @@ namespace UMI3DHub.Services
 
         #region Methods
 
+        /// <summary>
+        /// Gets last releases of a repository.
+        /// </summary>
+        /// <param name="repositoryOwner"></param>
+        /// <param name="repositoryName"></param>
+        /// <returns></returns>
         public static async Task<List<GithubReleaseModel>> GetReleasesAsync(string repositoryOwner, string repositoryName)
         {
             client.DefaultRequestHeaders.Add("User-Agent", "request");
@@ -46,6 +67,12 @@ namespace UMI3DHub.Services
             return releases;
         }
 
+        /// <summary>
+        /// Gets latest release of a repository.
+        /// </summary>
+        /// <param name="repositoryOwner"></param>
+        /// <param name="repositoryName"></param>
+        /// <returns></returns>
         public static async Task<GithubReleaseModel> GetLastReleaseAsync(string repositoryOwner, string repositoryName)
         {
             client.DefaultRequestHeaders.Add("User-Agent", "request");
@@ -67,6 +94,13 @@ namespace UMI3DHub.Services
             return release;
         }
 
+        /// <summary>
+        /// Download an exe associated to a Github release.
+        /// </summary>
+        /// <param name="release"></param>
+        /// <param name="onProgressChanged"></param>
+        /// <param name="onDownloadFinish"></param>
+        /// <param name="onDownloadFailed"></param>
         public static void DownloadRelease(GithubReleaseModel release, 
             Action<object, DownloadProgressChangedEventArgs> onProgressChanged,
             Action<string, int, string, object?, AsyncCompletedEventArgs> onDownloadFinish,
@@ -92,6 +126,11 @@ namespace UMI3DHub.Services
             });
         }
 
+        /// <summary>
+        /// Finds an exe file among Github release's assets.
+        /// </summary>
+        /// <param name="release"></param>
+        /// <returns></returns>
         private static GithubAssetModel? FindReleaseExecutable(GithubReleaseModel release)
         {
             if (release == null)
--- UMI3D-Hub/UMI3D-Hub/ViewModels/InstallSoftwareViewModel.cs
+++ UMI3D-Hub/UMI3D-Hub/ViewModels/InstallSoftwareViewModel.cs
@@ -1,4 +1,16 @@
-﻿using Avalonia.Controls.Selection;
+﻿/*
+Copyright 2022 Quentin Tran
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
@@ -223,6 +235,9 @@ namespace UMI3DHub.ViewModels
             }
         }
 
+        /// <summary>
+        /// Downloads chosen software version.
+        /// </summary>
         private void ChooseVersion()
         {
             if (SelectedVersion == null)
@@ -249,6 +264,14 @@ namespace UMI3DHub.ViewModels
             () => InstallInformation = "Download failed");
         }
 
+        /// <summary>
+        /// Installs downloaded software version.
+        /// </summary>
+        /// <param name="downloadedFile"></param>
+        /// <param name="version"></param>
+        /// <param name="githubId"></param>
+        /// <param name="isPrerelease"></param>
+        /// <exception cref="NotImplementedException"></exception>
         private async void InstallRelease(string downloadedFile, string version, int githubId, bool isPrerelease)
         {
             InstallInformation = "Installing ...";
